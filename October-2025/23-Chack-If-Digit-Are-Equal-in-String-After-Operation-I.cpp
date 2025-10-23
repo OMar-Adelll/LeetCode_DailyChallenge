a7cbaffa3cf0245648feda2c86997c81e5a1b5e8
Omar Adel <OmarAdel.connect@gmail.com>
2025-10-23 12:58:10 +0300

    23-October

---
+++ October-2025/23-Chack-If-Digit-Are-Equal-in-String-After-Operation-I.cpp
@@ -0,0 +1,46 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+class Solution
+{
+public:
+    bool hasSameDigits(string s)
+    {
+        auto valid = [&](string s) -> bool
+        {
+            for (int i = 1; i < sz(s); i++)
+                if (s[i] != s[i - 1])
+                    return 0;
+
+            return 1;
+        };
+
+        while (sz(s) > 2)
+        {
+            string word = "";
+            for (int i = 1; i < sz(s); i++)
+            {
+                int x = s[i] - '0', y = s[i - 1] - '0';
+                int rs = (x + y) % 10;
+                word += to_string(rs);
+            }
+
+            if (valid(word))
+                return 1;
+
+            s = word;
+        }
+        return 0;
+    }
+};
