3b9255c6c25d73bca97b01c3b6c54d33059ac9d7
Omar Adel <OmarAdel.connect@gmail.com>
2026-01-01 04:48:59 +0200

    [Easy]

---
+++ 2026/January/1-Plus-One.cpp
@@ -0,0 +1,33 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
