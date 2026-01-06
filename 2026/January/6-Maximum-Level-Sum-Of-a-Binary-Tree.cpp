3ac91aca483b7ac6edbe118766a45f0ce7fe17a3
Omar Adel <OmarAdel.connect@gmail.com>
2026-01-06 16:35:15 +0200

    [Medium]

---
+++ 2026/January/6-Maximum-Level-Sum-Of-a-Binary-Tree.cpp
@@ -0,0 +1,59 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int maxLevelSum(TreeNode *root)
+    {
+        int mxsum = INT_MIN, lv = 0, mxlv = 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            lv++;
+            int sum = 0, sz = q.size();
+            while (sz--)
+            {
+                auto node = q.front();
+                q.pop();
+                sum += node->val;
+
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+
+            if (sum > mxsum)
+            {
+                mxsum = sum;
+                mxlv = lv;
+            }
+        }
+
+        return mxlv;
+    }
+};
