bdff91f592baab3095047779a012ea3d314c3d50
Omar Adel <OmarAdel.connect@gmail.com>
2026-02-01 12:08:15 +0200

    [Easy]

---
+++ 2026/February/1-Divide-an-Array-Into-Subarrays-With-Minimum-Cost-I.cpp
@@ -0,0 +1,23 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int sum = nums[0];
+        sort(nums.begin() + 1,nums.end());
+        sum += nums[1] + nums[2];
+        return sum;
+    }
+};
