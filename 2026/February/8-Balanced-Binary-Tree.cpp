924cb97fc9d56282452f3ff5fe19bb37eda34994
Omar Adel <OmarAdel.connect@gmail.com>
2026-02-08 13:32:48 +0200

    [Easy]

---
+++ 2026/February/8-Balanced-Binary-Tree.cpp
@@ -0,0 +1,49 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    int depth(TreeNode *node)
+    {
+        if (!node)
+            return 0;
+        int l = depth(node->left);
+        if (l == -1)
+            return -1;
+        int r = depth(node->right);
+        if (r == -1)
+            return -1;
+
+        if (abs(r - l) > 1)
+            return -1;
+
+        return 1 + max(l, r);
+    }
+    bool isBalanced(TreeNode *root)
+    {
+        return depth(root) != -1;
+    }
+};
