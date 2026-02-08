f671d4340dbeba9d72821f937807b2c177dcb6da
Omar Adel <OmarAdel.connect@gmail.com>
2026-02-08 13:31:55 +0200

    [Medium]

---
+++ 2026/February/7-Minimum-Deletions-to-Make-String-Balanced.cpp
@@ -0,0 +1,31 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+class Solution
+{
+public:
+    int minimumDeletions(string s)
+    {
+        int dp = 0, cnt = 0;
+        for (auto &c : s)
+        {
+            if (c == 'b')
+                cnt++;
+            else
+                dp = min(dp + 1, cnt);
+        }
+
+        return dp;
+    }
+};
