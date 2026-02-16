169ab4ab8f6e04217160852d696c098f70668572
Omar Adel <OmarAdel.connect@gmail.com>
2026-02-16 14:49:12 +0200

    [Easy]

---
+++ 2026/February/16-Reverse-bits.cpp
@@ -0,0 +1,27 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(vec) (vec).begin(), (vec).end()
+#define sz(x) (int)(x).size()
+#define i(x) (x - 'a')
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define nl '\n'
+
+class Solution
+{
+public:
+    int reverseBits(int n)
+    {
+        bitset<32> bits = n;
+        bitset<32> ans;
+        for (int i = 0; i < 32; i++)
+            ans[i] = bits[31 - i];
+
+        return ans.to_ullong();
+    }
+};
