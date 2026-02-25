59f977c6c2c8f9cc4581c32b9775bd4fb2e2898b
Omar Adel <OmarAdel.connect@gmail.com>
2026-02-25 08:49:08 +0200

    [Easy]

---
+++ 2026/February/24-Sum-of-Root-To-Leaf-Binary-Numbers.cpp
@@ -0,0 +1,45 @@
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define i(x) (x - 'a')
+#define nl '\n'
+#define sz(x) (int)(x).size()
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define all(vec) (vec).begin(), (vec).end()
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+ 
+class Solution
+{
+public:
+    int dfs(TreeNode *node, int cur)
+    {
+        if (!node)
+            return 0;
+
+        cur = cur * 2 + node->val;
+
+        if (!node->left && !node->right)
+            return cur;
+
+        return dfs(node->left, cur) + dfs(node->right, cur);
+    }
+    int sumRootToLeaf(TreeNode *root) { return dfs(root, 0); }
+};
