dc3bd74cca8d5ea87cfb2003794efbb99a0e5f77
Omar Adel <OmarAdel.connect@gmail.com>
2026-03-04 05:36:01 +0200

    fix leetcode template

--- 2026/March/4-Special Positions in a Binary Matrix.cpp
+++ 2026/March/4-Special Positions in a Binary Matrix.cpp
@@ -1,38 +1,57 @@
-class Solution {
+// بسم الله الرحمن الرحيم
+//  وَأَنْ لَيْسَ لِلْإِنسَانِ إِلَّا مَا سَعَىٰ
+//  Free Palestine
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define i(x) (x - 'a')
+#define nl '\n'
+#define sz(x) (int)(x).size()
+#define updmax(a, x) (a = max((a), (x)))
+#define updmin(a, x) (a = min((a), (x)))
+#define all(vec) (vec).begin(), (vec).end()
+
+class Solution
+{
 public:
-    int numSpecial(vector<vector<int>>& mat) {
+    int numSpecial(vector<vector<int>> &mat)
+    {
         int cnt = 0, n = mat.size(), m = mat[0].size();
-        for(int i = 0 ;i < n;i++)
+        for (int i = 0; i < n; i++)
         {
-            for(int j = 0 ;j < m;j++)
+            for (int j = 0; j < m; j++)
             {
-                if(mat[i][j] == 1)
+                if (mat[i][j] == 1)
                 {
                     bool ok = true;
-                    for (int x = 0 ; x < n; x++)
+                    for (int x = 0; x < n; x++)
                     {
-                        if(x == i) continue;
-                        if(mat[x][j] == 1)
+                        if (x == i)
+                            continue;
+                        if (mat[x][j] == 1)
                         {
                             ok = false;
                             break;
                         }
                     }
 
-                    if(!ok) break;
+                    if (!ok)
+                        break;
 
-                    for(int y = 0 ; y < m ;y++)
+                    for (int y = 0; y < m; y++)
                     {
-                        if(y == j) continue;
-                        if(mat[i][y] == 1)
+                        if (y == j)
+                            continue;
+                        if (mat[i][y] == 1)
                         {
                             ok = false;
                             break;
                         }
                     }
 
-
-                    if(ok) cnt++;
+                    if (ok)
+                        cnt++;
                 }
             }
         }
